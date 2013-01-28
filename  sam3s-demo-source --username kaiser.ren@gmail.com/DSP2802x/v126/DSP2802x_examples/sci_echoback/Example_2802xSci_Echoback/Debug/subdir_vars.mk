65826ff96f5d0ece1e555849edb9bf736d3c7585
kaiser.ren <kaiser.ren@gmail.com>
2013-01-28 04:18:11 +0000

    C2000 DSP28027 1. NEW: add some new files.

---
+++  sam3s-demo-source --username kaiser.ren@gmail.com/DSP2802x/v126/DSP2802x_examples/sci_echoback/Example_2802xSci_Echoback/Debug/subdir_vars.mk
@@ -0,0 +1,84 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+D:/Firmware/sam3s-demo-source/trunk/\ sam3s-demo-source\ --username\ kaiser.ren@gmail.com/DSP2802x/v126/DSP2802x_common/cmd/28027_RAM_lnk.cmd \
+D:/Firmware/sam3s-demo-source/trunk/\ sam3s-demo-source\ --username\ kaiser.ren@gmail.com/DSP2802x/v126/DSP2802x_headers/cmd/DSP2802x_Headers_nonBIOS.cmd 
+
+ASM_SRCS += \
+D:/Firmware/sam3s-demo-source/trunk/\ sam3s-demo-source\ --username\ kaiser.ren@gmail.com/DSP2802x/v126/DSP2802x_common/source/DSP2802x_CodeStartBranch.asm \
+D:/Firmware/sam3s-demo-source/trunk/\ sam3s-demo-source\ --username\ kaiser.ren@gmail.com/DSP2802x/v126/DSP2802x_common/source/DSP2802x_usDelay.asm 
+
+C_SRCS += \
+D:/Firmware/sam3s-demo-source/trunk/\ sam3s-demo-source\ --username\ kaiser.ren@gmail.com/DSP2802x/v126/DSP2802x_common/source/DSP2802x_DefaultIsr.c \
+D:/Firmware/sam3s-demo-source/trunk/\ sam3s-demo-source\ --username\ kaiser.ren@gmail.com/DSP2802x/v126/DSP2802x_headers/source/DSP2802x_GlobalVariableDefs.c \
+D:/Firmware/sam3s-demo-source/trunk/\ sam3s-demo-source\ --username\ kaiser.ren@gmail.com/DSP2802x/v126/DSP2802x_common/source/DSP2802x_PieCtrl.c \
+D:/Firmware/sam3s-demo-source/trunk/\ sam3s-demo-source\ --username\ kaiser.ren@gmail.com/DSP2802x/v126/DSP2802x_common/source/DSP2802x_PieVect.c \
+D:/Firmware/sam3s-demo-source/trunk/\ sam3s-demo-source\ --username\ kaiser.ren@gmail.com/DSP2802x/v126/DSP2802x_common/source/DSP2802x_Sci.c \
+D:/Firmware/sam3s-demo-source/trunk/\ sam3s-demo-source\ --username\ kaiser.ren@gmail.com/DSP2802x/v126/DSP2802x_common/source/DSP2802x_SysCtrl.c \
+D:/Firmware/sam3s-demo-source/trunk/\ sam3s-demo-source\ --username\ kaiser.ren@gmail.com/DSP2802x/v126/DSP2802x_examples/sci_echoback/Example_2802xSci_Echoback.c 
+
+OBJS += \
+C:/tidcs/c28/DSP2802x/v126/DSP2802x_examples/sci_echoback/Debug/DSP2802x_CodeStartBranch.obj \
+C:/tidcs/c28/DSP2802x/v126/DSP2802x_examples/sci_echoback/Debug/DSP2802x_DefaultIsr.obj \
+C:/tidcs/c28/DSP2802x/v126/DSP2802x_examples/sci_echoback/Debug/DSP2802x_GlobalVariableDefs.obj \
+C:/tidcs/c28/DSP2802x/v126/DSP2802x_examples/sci_echoback/Debug/DSP2802x_PieCtrl.obj \
+C:/tidcs/c28/DSP2802x/v126/DSP2802x_examples/sci_echoback/Debug/DSP2802x_PieVect.obj \
+C:/tidcs/c28/DSP2802x/v126/DSP2802x_examples/sci_echoback/Debug/DSP2802x_Sci.obj \
+C:/tidcs/c28/DSP2802x/v126/DSP2802x_examples/sci_echoback/Debug/DSP2802x_SysCtrl.obj \
+C:/tidcs/c28/DSP2802x/v126/DSP2802x_examples/sci_echoback/Debug/DSP2802x_usDelay.obj \
+C:/tidcs/c28/DSP2802x/v126/DSP2802x_examples/sci_echoback/Debug/Example_2802xSci_Echoback.obj 
+
+ASM_DEPS += \
+./DSP2802x_CodeStartBranch.pp \
+./DSP2802x_usDelay.pp 
+
+C_DEPS += \
+./DSP2802x_DefaultIsr.pp \
+./DSP2802x_GlobalVariableDefs.pp \
+./DSP2802x_PieCtrl.pp \
+./DSP2802x_PieVect.pp \
+./DSP2802x_Sci.pp \
+./DSP2802x_SysCtrl.pp \
+./Example_2802xSci_Echoback.pp 
+
+C_DEPS__QUOTED += \
+"DSP2802x_DefaultIsr.pp" \
+"DSP2802x_GlobalVariableDefs.pp" \
+"DSP2802x_PieCtrl.pp" \
+"DSP2802x_PieVect.pp" \
+"DSP2802x_Sci.pp" \
+"DSP2802x_SysCtrl.pp" \
+"Example_2802xSci_Echoback.pp" 
+
+OBJS__QUOTED += \
+"C:\tidcs\c28\DSP2802x\v126\DSP2802x_examples\sci_echoback\Debug\DSP2802x_CodeStartBranch.obj" \
+"C:\tidcs\c28\DSP2802x\v126\DSP2802x_examples\sci_echoback\Debug\DSP2802x_DefaultIsr.obj" \
+"C:\tidcs\c28\DSP2802x\v126\DSP2802x_examples\sci_echoback\Debug\DSP2802x_GlobalVariableDefs.obj" \
+"C:\tidcs\c28\DSP2802x\v126\DSP2802x_examples\sci_echoback\Debug\DSP2802x_PieCtrl.obj" \
+"C:\tidcs\c28\DSP2802x\v126\DSP2802x_examples\sci_echoback\Debug\DSP2802x_PieVect.obj" \
+"C:\tidcs\c28\DSP2802x\v126\DSP2802x_examples\sci_echoback\Debug\DSP2802x_Sci.obj" \
+"C:\tidcs\c28\DSP2802x\v126\DSP2802x_examples\sci_echoback\Debug\DSP2802x_SysCtrl.obj" \
+"C:\tidcs\c28\DSP2802x\v126\DSP2802x_examples\sci_echoback\Debug\DSP2802x_usDelay.obj" \
+"C:\tidcs\c28\DSP2802x\v126\DSP2802x_examples\sci_echoback\Debug\Example_2802xSci_Echoback.obj" 
+
+ASM_DEPS__QUOTED += \
+"DSP2802x_CodeStartBranch.pp" \
+"DSP2802x_usDelay.pp" 
+
+ASM_SRCS__QUOTED += \
+"D:/Firmware/sam3s-demo-source/trunk/ sam3s-demo-source --username kaiser.ren@gmail.com/DSP2802x/v126/DSP2802x_common/source/DSP2802x_CodeStartBranch.asm" \
+"D:/Firmware/sam3s-demo-source/trunk/ sam3s-demo-source --username kaiser.ren@gmail.com/DSP2802x/v126/DSP2802x_common/source/DSP2802x_usDelay.asm" 
+
+C_SRCS__QUOTED += \
+"D:/Firmware/sam3s-demo-source/trunk/ sam3s-demo-source --username kaiser.ren@gmail.com/DSP2802x/v126/DSP2802x_common/source/DSP2802x_DefaultIsr.c" \
+"D:/Firmware/sam3s-demo-source/trunk/ sam3s-demo-source --username kaiser.ren@gmail.com/DSP2802x/v126/DSP2802x_headers/source/DSP2802x_GlobalVariableDefs.c" \
+"D:/Firmware/sam3s-demo-source/trunk/ sam3s-demo-source --username kaiser.ren@gmail.com/DSP2802x/v126/DSP2802x_common/source/DSP2802x_PieCtrl.c" \
+"D:/Firmware/sam3s-demo-source/trunk/ sam3s-demo-source --username kaiser.ren@gmail.com/DSP2802x/v126/DSP2802x_common/source/DSP2802x_PieVect.c" \
+"D:/Firmware/sam3s-demo-source/trunk/ sam3s-demo-source --username kaiser.ren@gmail.com/DSP2802x/v126/DSP2802x_common/source/DSP2802x_Sci.c" \
+"D:/Firmware/sam3s-demo-source/trunk/ sam3s-demo-source --username kaiser.ren@gmail.com/DSP2802x/v126/DSP2802x_common/source/DSP2802x_SysCtrl.c" \
+"D:/Firmware/sam3s-demo-source/trunk/ sam3s-demo-source --username kaiser.ren@gmail.com/DSP2802x/v126/DSP2802x_examples/sci_echoback/Example_2802xSci_Echoback.c" 
+
+
