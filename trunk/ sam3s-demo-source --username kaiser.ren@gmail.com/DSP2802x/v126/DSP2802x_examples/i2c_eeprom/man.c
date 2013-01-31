b2c55ab334811ff132e3df7c8868a37033fb9322
kaiser.ren@gmail.com <kaiser.ren@gmail.com@e6d403c7-af67-c1a6-3a8a-7b13f4292565>
2013-01-31 02:52:05 +0000

    C2000 DSP28027 1. CHG: move i2c part to ad5933.c

--- trunk/ sam3s-demo-source --username kaiser.ren@gmail.com/DSP2802x/v126/DSP2802x_examples/i2c_eeprom/man.c
+++ trunk/ sam3s-demo-source --username kaiser.ren@gmail.com/DSP2802x/v126/DSP2802x_examples/i2c_eeprom/man.c
@@ -65,44 +65,21 @@
 //###########################################################################
 
 #include "DSP28x_Project.h"     // Device Headerfile and Examples Include File
-
+#include "ad5933.h"
 // Note: I2C Macros used in this example can be found in the
 // DSP2802x_I2C_defines.h file
 
 // Prototype statements for functions found within this file.
-void   I2CA_Init(void);
-Uint16 I2CA_WriteData(struct I2CMSG *msg);
-Uint16 I2CA_ReadData(struct I2CMSG *msg);
-interrupt void i2c_int1a_isr(void);
-void pass(void);
-void fail(void);
 
-#define I2C_SLAVE_ADDR        0x0d
-#define I2C_NUMBYTES          2
-#define I2C_EEPROM_HIGH_ADDR  0x00
-#define I2C_EEPROM_LOW_ADDR   0x30
+//void pass(void);
+//void fail(void);
+
+
 
 // Global variables
 // Two bytes will be used for the outgoing address,
 // thus only setup 14 bytes maximum
-struct I2CMSG I2cMsgOut1={I2C_MSGSTAT_SEND_WITHSTOP,
-                          I2C_SLAVE_ADDR,
-                          I2C_NUMBYTES,
-                          I2C_EEPROM_HIGH_ADDR,
-                          I2C_EEPROM_LOW_ADDR,
-                          0x12,                   // Msg Byte 1
-                          0x34};                  // Msg Byte 2
 
-
-struct I2CMSG I2cMsgIn1={ I2C_MSGSTAT_SEND_NOSTOP,
-                          I2C_SLAVE_ADDR,
-                          I2C_NUMBYTES,
-                          I2C_EEPROM_HIGH_ADDR,
-                          I2C_EEPROM_LOW_ADDR};
-
-struct I2CMSG *CurrentMsgPtr;               // Used in interrupts
-Uint16 PassCount;
-Uint16 FailCount;
 
 void main(void)
 {
@@ -248,199 +225,10 @@ void main(void)
    }   // end of for(;;)
 }   // end of main
 
-void I2CA_Init(void)
-{
-   // Initialize I2C
-   I2caRegs.I2CSAR = 0x0050;        // Slave address - EEPROM control code
-   // I2CCLK = SYSCLK/(I2CPSC+1)
-   #if (CPU_FRQ_40MHZ||CPU_FRQ_50MHZ)
-     I2caRegs.I2CPSC.all = 4;       // Prescaler - need 7-12 Mhz on module clk
-   #endif
 
-   #if (CPU_FRQ_60MHZ)
-     I2caRegs.I2CPSC.all = 6;       // Prescaler - need 7-12 Mhz on module clk
-   #endif
-   I2caRegs.I2CCLKL = 10;           // NOTE: must be non zero
-   I2caRegs.I2CCLKH = 5;            // NOTE: must be non zero
-   I2caRegs.I2CIER.all = 0x24;      // Enable SCD & ARDY interrupts
 
-   I2caRegs.I2CMDR.all = 0x0020;    // Take I2C out of reset
-                                    // Stop I2C when suspended
 
-   I2caRegs.I2CFFTX.all = 0x6000;   // Enable FIFO mode and TXFIFO
-   I2caRegs.I2CFFRX.all = 0x2040;   // Enable RXFIFO, clear RXFFINT,
-
-   return;
-}
-
-Uint16 I2CA_WriteData(struct I2CMSG *msg)
-{
-   Uint16 i;
-
-   // Wait until the STP bit is cleared from any previous master communication.
-   // Clearing of this bit by the module is delayed until after the SCD bit is
-   // set. If this bit is not checked prior to initiating a new message, the
-   // I2C could get confused.
-   if (I2caRegs.I2CMDR.bit.STP == 1)
-   {
-      return I2C_STP_NOT_READY_ERROR;
-   }
-
-   // Setup slave address
-   I2caRegs.I2CSAR = msg->SlaveAddress;
-
-   // Check if bus busy
-   if (I2caRegs.I2CSTR.bit.BB == 1)
-   {
-      return I2C_BUS_BUSY_ERROR;
-   }
-
-   // Setup number of bytes to send
-   // MsgBuffer + Address
-   I2caRegs.I2CCNT = msg->NumOfBytes;
-
-   // Setup data to send
-   //I2caRegs.I2CDXR = msg->MemoryHighAddr;
-   //I2caRegs.I2CDXR = msg->MemoryLowAddr;
-// for (i=0; i<msg->NumOfBytes-2; i++)
-   for (i=0; i<msg->NumOfBytes; i++)
-
-   {
-      I2caRegs.I2CDXR = *(msg->MsgBuffer+i);
-   }
-
-   // Send start as master transmitter
-   I2caRegs.I2CMDR.all = 0x6E20;
-
-   return I2C_SUCCESS;
-}
-
-Uint16 I2CA_ReadData(struct I2CMSG *msg)
-{
-   // Wait until the STP bit is cleared from any previous master communication.
-   // Clearing of this bit by the module is delayed until after the SCD bit is
-   // set. If this bit is not checked prior to initiating a new message, the
-   // I2C could get confused.
-   if (I2caRegs.I2CMDR.bit.STP == 1)
-   {
-      return I2C_STP_NOT_READY_ERROR;
-   }
-
-   I2caRegs.I2CSAR = msg->SlaveAddress;
-
-   if(msg->MsgStatus == I2C_MSGSTAT_SEND_NOSTOP)
-   {
-      // Check if bus busy
-      if (I2caRegs.I2CSTR.bit.BB == 1)
-      {
-         return I2C_BUS_BUSY_ERROR;
-      }
-      I2caRegs.I2CCNT = 2;
-      I2caRegs.I2CDXR = msg->MemoryHighAddr;
-      I2caRegs.I2CDXR = msg->MemoryLowAddr;
-      I2caRegs.I2CMDR.all = 0x2620;         // Send data to setup EEPROM address
-   }
-   else if(msg->MsgStatus == I2C_MSGSTAT_RESTART)
-   {
-      I2caRegs.I2CCNT = msg->NumOfBytes;    // Setup how many bytes to expect
-      I2caRegs.I2CMDR.all = 0x2C20;         // Send restart as master receiver
-   }
-
-   return I2C_SUCCESS;
-}
-
-interrupt void i2c_int1a_isr(void)     // I2C-A
-{
-   Uint16 IntSource, i;
-
-   // Read interrupt source
-   IntSource = I2caRegs.I2CISRC.all;
-
-   // Interrupt source = stop condition detected
-   if(IntSource == I2C_SCD_ISRC)
-   {
-      // If completed message was writing data, reset msg to inactive state
-      if (CurrentMsgPtr->MsgStatus == I2C_MSGSTAT_WRITE_BUSY)
-      {
-         CurrentMsgPtr->MsgStatus = I2C_MSGSTAT_INACTIVE;
-      }
-      else
-      {
-         // If a message receives a NACK during the address setup portion of the
-         // EEPROM read, the code further below included in the register access ready
-         // interrupt source code will generate a stop condition. After the stop
-         // condition is received (here), set the message status to try again.
-         // User may want to limit the number of retries before generating an error.
-         if(CurrentMsgPtr->MsgStatus == I2C_MSGSTAT_SEND_NOSTOP_BUSY)
-         {
-            CurrentMsgPtr->MsgStatus = I2C_MSGSTAT_SEND_NOSTOP;
-         }
-         // If completed message was reading EEPROM data, reset msg to inactive state
-         // and read data from FIFO.
-         else if (CurrentMsgPtr->MsgStatus == I2C_MSGSTAT_READ_BUSY)
-         {
-            CurrentMsgPtr->MsgStatus = I2C_MSGSTAT_INACTIVE;
-            for(i=0; i < I2C_NUMBYTES; i++)
-            {
-              CurrentMsgPtr->MsgBuffer[i] = I2caRegs.I2CDRR;
-            }
-         {
-         // Check recieved data
-         for(i=0; i < I2C_NUMBYTES; i++)
-         {
-            if(I2cMsgIn1.MsgBuffer[i] == I2cMsgOut1.MsgBuffer[i])
-            {
-                PassCount++;
-            }
-            else
-            {
-                FailCount++;
-            }
-         }
-         if(PassCount == I2C_NUMBYTES)
-         {
-            pass();
-         }
-         else
-         {
-            fail();
-         }
-
-      }
-
-    }
-      }
-   }  // end of stop condition detected
-
-   // Interrupt source = Register Access Ready
-   // This interrupt is used to determine when the EEPROM address setup portion of the
-   // read data communication is complete. Since no stop bit is commanded, this flag
-   // tells us when the message has been sent instead of the SCD flag. If a NACK is
-   // received, clear the NACK bit and command a stop. Otherwise, move on to the read
-   // data portion of the communication.
-   else if(IntSource == I2C_ARDY_ISRC)
-   {
-      if(I2caRegs.I2CSTR.bit.NACK == 1)
-      {
-         I2caRegs.I2CMDR.bit.STP = 1;
-         I2caRegs.I2CSTR.all = I2C_CLR_NACK_BIT;
-      }
-      else if(CurrentMsgPtr->MsgStatus == I2C_MSGSTAT_SEND_NOSTOP_BUSY)
-      {
-         CurrentMsgPtr->MsgStatus = I2C_MSGSTAT_RESTART;
-      }
-   }  // end of register access ready
-
-   else
-   {
-      // Generate some error due to invalid interrupt source
-      asm("   ESTOP0");
-   }
-
-   // Enable future I2C (PIE Group 8) interrupts
-   PieCtrlRegs.PIEACK.all = PIEACK_GROUP8;
-}
-
+/*
 void pass()
 {
     asm("   ESTOP0");
@@ -452,7 +240,7 @@ void fail()
     asm("   ESTOP0");
     for(;;);
 }
-
+*/
 //===========================================================================
 // No more.
 //===========================================================================
