f7b8723669e2c6ebad2d32784dbb6bf0c798108c
kaiser.ren@gmail.com <kaiser.ren@gmail.com@e6d403c7-af67-c1a6-3a8a-7b13f4292565>
2013-02-23 06:25:56 +0000

    C2000 DSP28027 1. CHG: add ad5933 source code into Example_F2802xLaunchPadDemo.

--- trunk/ sam3s-demo-source --username kaiser.ren@gmail.com/DSP2802x/C2000_LaunchPad/f2802x_examples/C2kLaunchPadDemo/Flash/subdir_vars.mk
+++ trunk/ sam3s-demo-source --username kaiser.ren@gmail.com/DSP2802x/C2000_LaunchPad/f2802x_examples/C2kLaunchPadDemo/Flash/subdir_vars.mk
@@ -9,22 +9,120 @@ F:/\ Firmware/sam3s-demo-source/trunk/\ sam3s-demo-source\ --username\ kaiser.re
 LIB_SRCS += \
 F:/\ Firmware/sam3s-demo-source/trunk/\ sam3s-demo-source\ --username\ kaiser.ren@gmail.com/DSP2802x/C2000_LaunchPad/f2802x_common/lib/driverlib.lib 
 
+ASM_SRCS += \
+F:/\ Firmware/sam3s-demo-source/trunk/\ sam3s-demo-source\ --username\ kaiser.ren@gmail.com/DSP2802x/C2000_LaunchPad/f2802x_common/source/F2802x_CodeStartBranch.asm \
+F:/\ Firmware/sam3s-demo-source/trunk/\ sam3s-demo-source\ --username\ kaiser.ren@gmail.com/DSP2802x/C2000_LaunchPad/f2802x_common/source/F2802x_usDelay.asm 
+
 C_SRCS += \
-../Example_F2802xLaunchPadDemo.c 
+../Communication.c \
+F:/\ Firmware/sam3s-demo-source/trunk/\ sam3s-demo-source\ --username\ kaiser.ren@gmail.com/DSP2802x/C2000_LaunchPad/f2802x_common/source/F2802x_CpuTimers.c \
+F:/\ Firmware/sam3s-demo-source/trunk/\ sam3s-demo-source\ --username\ kaiser.ren@gmail.com/DSP2802x/C2000_LaunchPad/f2802x_common/source/F2802x_DefaultIsr.c \
+F:/\ Firmware/sam3s-demo-source/trunk/\ sam3s-demo-source\ --username\ kaiser.ren@gmail.com/DSP2802x/C2000_LaunchPad/f2802x_headers/source/F2802x_GlobalVariableDefs.c \
+F:/\ Firmware/sam3s-demo-source/trunk/\ sam3s-demo-source\ --username\ kaiser.ren@gmail.com/DSP2802x/C2000_LaunchPad/f2802x_common/source/F2802x_Gpio.c \
+F:/\ Firmware/sam3s-demo-source/trunk/\ sam3s-demo-source\ --username\ kaiser.ren@gmail.com/DSP2802x/C2000_LaunchPad/f2802x_common/source/F2802x_I2C.c \
+F:/\ Firmware/sam3s-demo-source/trunk/\ sam3s-demo-source\ --username\ kaiser.ren@gmail.com/DSP2802x/C2000_LaunchPad/f2802x_common/source/F2802x_PieCtrl.c \
+F:/\ Firmware/sam3s-demo-source/trunk/\ sam3s-demo-source\ --username\ kaiser.ren@gmail.com/DSP2802x/C2000_LaunchPad/f2802x_common/source/F2802x_PieVect.c \
+F:/\ Firmware/sam3s-demo-source/trunk/\ sam3s-demo-source\ --username\ kaiser.ren@gmail.com/DSP2802x/C2000_LaunchPad/f2802x_common/source/F2802x_Sci.c \
+F:/\ Firmware/sam3s-demo-source/trunk/\ sam3s-demo-source\ --username\ kaiser.ren@gmail.com/DSP2802x/C2000_LaunchPad/f2802x_common/source/F2802x_SysCtrl.c \
+../ad5933.c \
+../calc.c \
+../com.c \
+../main.c 
 
 OBJS += \
-./Example_F2802xLaunchPadDemo.obj 
+./Communication.obj \
+./F2802x_CodeStartBranch.obj \
+./F2802x_CpuTimers.obj \
+./F2802x_DefaultIsr.obj \
+./F2802x_GlobalVariableDefs.obj \
+./F2802x_Gpio.obj \
+./F2802x_I2C.obj \
+./F2802x_PieCtrl.obj \
+./F2802x_PieVect.obj \
+./F2802x_Sci.obj \
+./F2802x_SysCtrl.obj \
+./F2802x_usDelay.obj \
+./ad5933.obj \
+./calc.obj \
+./com.obj \
+./main.obj 
+
+ASM_DEPS += \
+./F2802x_CodeStartBranch.pp \
+./F2802x_usDelay.pp 
 
 C_DEPS += \
-./Example_F2802xLaunchPadDemo.pp 
+./Communication.pp \
+./F2802x_CpuTimers.pp \
+./F2802x_DefaultIsr.pp \
+./F2802x_GlobalVariableDefs.pp \
+./F2802x_Gpio.pp \
+./F2802x_I2C.pp \
+./F2802x_PieCtrl.pp \
+./F2802x_PieVect.pp \
+./F2802x_Sci.pp \
+./F2802x_SysCtrl.pp \
+./ad5933.pp \
+./calc.pp \
+./com.pp \
+./main.pp 
 
 C_DEPS__QUOTED += \
-"Example_F2802xLaunchPadDemo.pp" 
+"Communication.pp" \
+"F2802x_CpuTimers.pp" \
+"F2802x_DefaultIsr.pp" \
+"F2802x_GlobalVariableDefs.pp" \
+"F2802x_Gpio.pp" \
+"F2802x_I2C.pp" \
+"F2802x_PieCtrl.pp" \
+"F2802x_PieVect.pp" \
+"F2802x_Sci.pp" \
+"F2802x_SysCtrl.pp" \
+"ad5933.pp" \
+"calc.pp" \
+"com.pp" \
+"main.pp" 
 
 OBJS__QUOTED += \
-"Example_F2802xLaunchPadDemo.obj" 
+"Communication.obj" \
+"F2802x_CodeStartBranch.obj" \
+"F2802x_CpuTimers.obj" \
+"F2802x_DefaultIsr.obj" \
+"F2802x_GlobalVariableDefs.obj" \
+"F2802x_Gpio.obj" \
+"F2802x_I2C.obj" \
+"F2802x_PieCtrl.obj" \
+"F2802x_PieVect.obj" \
+"F2802x_Sci.obj" \
+"F2802x_SysCtrl.obj" \
+"F2802x_usDelay.obj" \
+"ad5933.obj" \
+"calc.obj" \
+"com.obj" \
+"main.obj" 
+
+ASM_DEPS__QUOTED += \
+"F2802x_CodeStartBranch.pp" \
+"F2802x_usDelay.pp" 
 
 C_SRCS__QUOTED += \
-"../Example_F2802xLaunchPadDemo.c" 
+"../Communication.c" \
+"F:/ Firmware/sam3s-demo-source/trunk/ sam3s-demo-source --username kaiser.ren@gmail.com/DSP2802x/C2000_LaunchPad/f2802x_common/source/F2802x_CpuTimers.c" \
+"F:/ Firmware/sam3s-demo-source/trunk/ sam3s-demo-source --username kaiser.ren@gmail.com/DSP2802x/C2000_LaunchPad/f2802x_common/source/F2802x_DefaultIsr.c" \
+"F:/ Firmware/sam3s-demo-source/trunk/ sam3s-demo-source --username kaiser.ren@gmail.com/DSP2802x/C2000_LaunchPad/f2802x_headers/source/F2802x_GlobalVariableDefs.c" \
+"F:/ Firmware/sam3s-demo-source/trunk/ sam3s-demo-source --username kaiser.ren@gmail.com/DSP2802x/C2000_LaunchPad/f2802x_common/source/F2802x_Gpio.c" \
+"F:/ Firmware/sam3s-demo-source/trunk/ sam3s-demo-source --username kaiser.ren@gmail.com/DSP2802x/C2000_LaunchPad/f2802x_common/source/F2802x_I2C.c" \
+"F:/ Firmware/sam3s-demo-source/trunk/ sam3s-demo-source --username kaiser.ren@gmail.com/DSP2802x/C2000_LaunchPad/f2802x_common/source/F2802x_PieCtrl.c" \
+"F:/ Firmware/sam3s-demo-source/trunk/ sam3s-demo-source --username kaiser.ren@gmail.com/DSP2802x/C2000_LaunchPad/f2802x_common/source/F2802x_PieVect.c" \
+"F:/ Firmware/sam3s-demo-source/trunk/ sam3s-demo-source --username kaiser.ren@gmail.com/DSP2802x/C2000_LaunchPad/f2802x_common/source/F2802x_Sci.c" \
+"F:/ Firmware/sam3s-demo-source/trunk/ sam3s-demo-source --username kaiser.ren@gmail.com/DSP2802x/C2000_LaunchPad/f2802x_common/source/F2802x_SysCtrl.c" \
+"../ad5933.c" \
+"../calc.c" \
+"../com.c" \
+"../main.c" 
+
+ASM_SRCS__QUOTED += \
+"F:/ Firmware/sam3s-demo-source/trunk/ sam3s-demo-source --username kaiser.ren@gmail.com/DSP2802x/C2000_LaunchPad/f2802x_common/source/F2802x_CodeStartBranch.asm" \
+"F:/ Firmware/sam3s-demo-source/trunk/ sam3s-demo-source --username kaiser.ren@gmail.com/DSP2802x/C2000_LaunchPad/f2802x_common/source/F2802x_usDelay.asm" 
 
 
