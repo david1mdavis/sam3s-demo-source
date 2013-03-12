f1ddad0511dc52e685734e9551f721d82b261739
kaiser.ren@gmail.com <kaiser.ren@gmail.com@e6d403c7-af67-c1a6-3a8a-7b13f4292565>
2013-03-12 13:19:20 +0000

    tag for i2c_ad5933 V0.03. This is a tag for a complete solution.

---
+++ tags/i2c_ad5933_v0.03/DSP2802x_examples/i2c_eeprom/calc.h
@@ -0,0 +1,13 @@
+/*
+ * calc.h
+ *
+ *  Created on: 2012-11-26
+ *      Author: cui
+ */
+
+#ifndef CALC_H_
+#define CALC_H_
+
+extern const double mag_ref[AD5933_BOARD_CNT_ICMT];
+
+#endif /* CALC_H_ */
