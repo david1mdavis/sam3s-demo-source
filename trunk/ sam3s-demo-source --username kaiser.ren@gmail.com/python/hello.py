d87fa2e993684221f9bdaaf304a7632900eaef7e
kaiser.ren@gmail.com <kaiser.ren@gmail.com@e6d403c7-af67-c1a6-3a8a-7b13f4292565>
2013-09-11 07:28:40 +0000

    python 1. NEW: add learning_python_the_hard_way folder.

--- trunk/ sam3s-demo-source --username kaiser.ren@gmail.com/python/hello.py
+++ trunk/ sam3s-demo-source --username kaiser.ren@gmail.com/python/hello.py
@@ -1,4 +1,17 @@
 #!/usr/bin/python
 # Filename : hello.py
-print ("hello world")
 
+# multi-line test
+print ('''This is a multi-line string. This is the first line.
+This is the second line.
+"What's your name?," I asked.
+He said 'Bond, James Bond." ok.''')
+
+#/ test
+print('What\'s your name. \\')
+
+#/ test
+print(r'What\'s your name. \\')
+
+#/test
+print('Psiphon 3 là một công cụ giải quyết của Psiphon Inc. ứng dụng công nghệ VPN, SSH')
---
+++ trunk/ sam3s-demo-source --username kaiser.ren@gmail.com/python/string.py
@@ -0,0 +1,8 @@
+#! /usr/bin/python  
+a=2 
+b="test" 
+c=str(a)+b 
+d="1111" 
+e=a+int(d) 
+#How to print multiply values 
+print ("c is %s,e is %i" % (c,e)) 
