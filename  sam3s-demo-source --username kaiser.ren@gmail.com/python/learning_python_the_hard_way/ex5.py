33b74e965c33a1630f63c74e0b7ab1a3d263260c
kaiser.ren <kaiser.ren@gmail.com>
2013-09-12 08:10:02 +0000

    python 1. NEW: create example.

---
+++  sam3s-demo-source --username kaiser.ren@gmail.com/python/learning_python_the_hard_way/ex5.py
@@ -0,0 +1,14 @@
+my_name = "Zed A. shaw"
+my_age = 35 # not a lie
+my_height = 75 #inches
+my_weight = 180 #lbs
+my_eyes = 'blue'
+my_teeth = 'white'
+my_hair = 'brown'
+
+print "Let's talk about %s." %my_name
+print 'he\'s %d inches tall.' %my_height
+print "he's %d pounds heavy" %my_weight
+print"Actually that's not too heavy"
+print 'he\'s got %s eyes and %s hair.' %(my_eyes ,my_hair)
+print "Let's talk about %r happy." %my_name
