d17201f9aefcf3b5ab6ac7cf644b1e6b8c2bb56b
kaiser.ren@gmail.com <kaiser.ren@gmail.com@e6d403c7-af67-c1a6-3a8a-7b13f4292565>
2013-09-12 06:50:36 +0000

    python 1. NEW: ex6.py ~ ex10.py

---
+++ trunk/ sam3s-demo-source --username kaiser.ren@gmail.com/python/learning_python_the_hard_way/ex9.py
@@ -0,0 +1,16 @@
+#Exercise 9: Printing, Printing, Printing
+
+#here's some new strange stuff, remember type is exactly.
+days = "mon tue wed thu fri sat sun"
+months  = "\njan\nfeb\nmar\napr\nmay\njun\njul\naug\n"
+number =123.456
+
+print("here are the days: " , days)
+print("here are the months: " ,months)
+print("here are the numbers:",number, "right?")
+
+print ("""
+there's something going on here.
+with the three double-quotes.
+we'll be able to type as much as we like.
+    """)
