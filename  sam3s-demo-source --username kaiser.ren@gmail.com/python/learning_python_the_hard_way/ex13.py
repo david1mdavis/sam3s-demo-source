68572899bee5edeac18e1776a3ed276fd8018469
kaiser.ren <kaiser.ren@gmail.com>
2013-09-13 07:24:35 +0000

    python 1. NEW: ex13.py ~ ex15.py

---
+++  sam3s-demo-source --username kaiser.ren@gmail.com/python/learning_python_the_hard_way/ex13.py
@@ -0,0 +1,14 @@
+#Exercise 13: Parameters, Unpacking, Variables
+
+from sys import argv
+
+script, first, second, third = argv
+#script = argv
+
+print("the script is called:", script)
+print ("your first variable is:", first)
+print ("your second variable is:", second)
+print("your third variable is:", third)
+
+mcu = input("Input MCU type:"),
+print("%s" %mcu)
