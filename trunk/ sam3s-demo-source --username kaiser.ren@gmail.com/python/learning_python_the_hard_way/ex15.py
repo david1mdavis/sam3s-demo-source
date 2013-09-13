bc56b589473c8ecdc2ee6b0e8f701187322b8e20
kaiser.ren@gmail.com <kaiser.ren@gmail.com@e6d403c7-af67-c1a6-3a8a-7b13f4292565>
2013-09-13 07:24:35 +0000

    python 1. NEW: ex13.py ~ ex15.py

---
+++ trunk/ sam3s-demo-source --username kaiser.ren@gmail.com/python/learning_python_the_hard_way/ex15.py
@@ -0,0 +1,17 @@
+#Exercise 15: Reading Files
+
+from sys import argv
+
+script, filename = argv
+
+txt = open(filename)
+
+print ("Here's are your file %r" %filename)
+print (txt.read())
+
+print ("I'll also ask you to type it again.:")
+file_again = input('> ')
+
+txt_again = open(file_again)
+
+print (txt_again.read())
