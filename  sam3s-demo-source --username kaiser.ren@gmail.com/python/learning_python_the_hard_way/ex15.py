6c84ea36002b95269bbe5f1e2d92008dd6f3b8c4
kaiser.ren <kaiser.ren@gmail.com>
2013-09-16 06:11:41 +0000

    python 1. NEW: ex15.py ~ ex17.py

---  sam3s-demo-source --username kaiser.ren@gmail.com/python/learning_python_the_hard_way/ex15.py
+++  sam3s-demo-source --username kaiser.ren@gmail.com/python/learning_python_the_hard_way/ex15.py
@@ -1,13 +1,15 @@
 #Exercise 15: Reading Files
 
-from sys import argv
+#from sys import argv
 
-script, filename = argv
+#script, filename = argv
+
+filename = input('<')
 
 txt = open(filename)
 
 print ("Here's are your file %r" %filename)
-print (txt.read())
+print (txt.read(10)) #read ten characters
 
 print ("I'll also ask you to type it again.:")
 file_again = input('> ')
